53cf6534d100c9216b937bbdc126f9c5d5975341
Lavdim Imeri <lavdi-imeri@hotmail.com>
2018-11-21 13:16:29 +0100

    code added

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] sentence = {"This",  "sentence",  "is", "stored",  "in",  "an",  "array"};
+	for (String s : sentence) {
+        System.out.print(s + " ");
+
+	    }
+
+    }
+}
